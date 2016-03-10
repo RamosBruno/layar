54bf8b3fd2432b3bd2fac0011474460104e17344
Seijitsu <seijitsu_75@hotmail.fr>
2016-03-10 22:56:51 +0100

    header viex

--- admin/layout/header.php
+++ admin/layout/header.php
@@ -5,6 +5,9 @@
   <title>Best Western Augmented Reality</title>
   <link rel="stylesheet" href="AdminLTE-2.3.0/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <?php 
+    $url = $_SERVER['REQUEST_URI'];
+    if($url[1] != 'views'): ?>
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- daterange picker -->
@@ -26,9 +29,36 @@
   <link rel="stylesheet" href="public/css/style.css">
   <link rel="stylesheet" href="public/css/normalize.css">
   <link rel="icon" type="image/png" href="public/img/best_western_b.png" />
+  <?php else: ?>
+      <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- daterange picker -->
+  <link rel="stylesheet" href="../AdminLTE-2.3.0/plugins/daterangepicker/daterangepicker-bs3.css">
+  <!-- iCheck for checkboxes and radio inputs -->
+  <link rel="stylesheet" href="../AdminLTE-2.3.0/plugins/iCheck/all.css">
+  <!-- Bootstrap Color Picker -->
+  <link rel="stylesheet" href="../AdminLTE-2.3.0/plugins/colorpicker/bootstrap-colorpicker.min.css">
+  <!-- Bootstrap time Picker -->
+  <link rel="stylesheet" href="../AdminLTE-2.3.0/plugins/timepicker/bootstrap-timepicker.min.css">
+  <!-- Select2 -->
+  <link rel="stylesheet" href="../AdminLTE-2.3.0/plugins/select2/select2.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../AdminLTE-2.3.0/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+     folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="../AdminLTE-2.3.0/dist/css/skins/_all-skins.min.css">
+  <link rel="stylesheet" href="../public/css/style.css">
+  <link rel="stylesheet" href="../public/css/normalize.css">
+  <link rel="icon" type="image/png" href="public/img/best_western_b.png" />
+  <?php endif ?>
   <script src="//code.jquery.com/jquery-2.2.1.min.js"></script>
   <script src="//code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-  <script type="text/javascript" src="public/js/script.js"></script>
+  <?php if($url[1] != 'views'): ?>
+    <script type="text/javascript" src="public/js/script.js"></script>
+  <?php else: ?>  
+    <script type="text/javascript" src="../public/js/script.js"></script>
+  <?php endif ?>  
 </head>
 <body>
 	<header class="bg-blue">
